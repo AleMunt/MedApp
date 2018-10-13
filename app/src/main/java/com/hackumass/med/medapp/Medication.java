a9fb0e8a49786031bc86a2be38beb5692b6b9b5f
Alexandru Munteanu <munt.alexandru@gmail.com>
2018-10-13 18:39:09 -0400

    Removed frequency from medication

--- app/src/main/java/com/hackumass/med/medapp/Medication.java
+++ app/src/main/java/com/hackumass/med/medapp/Medication.java
@@ -4,15 +4,11 @@ public class Medication {
 
     private String brandName;
     private int dosage;
-    private int frequency;
-    private boolean perDay;
 
-    public Medication(String name, int dose, int freq, boolean day)
+    public Medication(String name, int dose)
     {
         this.brandName=name;
         this.dosage=dose;
-        this.frequency=freq;
-        this.perDay=day;
     }
 
     public int getDosage() {
@@ -21,13 +17,5 @@ public class Medication {
 
     public String getBrandName() {
         return brandName;
-    }
-
-    public boolean isPerDay() {
-        return perDay;
-    }
-
-    public int getFrequency() {
-        return frequency;
     }
 }
